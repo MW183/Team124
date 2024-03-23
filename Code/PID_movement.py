85fbe7d86510b9d71f1cce351e1017759703239e
MW183 <mikeweinstein183@gmail.com>
2024-03-23 15:13:21 -0400

    Uhh...lots of things.

--- Code/PID_movement.py
+++ Code/PID_movement.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_B, MoveDifferential, SpeedRPM
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedRPS, follow_for_ms
 from ev3dev2.sensor.lego import GyroSensor
-from ev3dev2.wheel import EV3EducationSetTire
-from ev3dev2.sensor import INPUT_1
-from time import sleep
 
+# Instantiate the MoveTank object
+tank = MoveTank(OUTPUT_B, OUTPUT_A) #left, right
+# Initialize the tank's gyro sensor
+tank.gyro = GyroSensor()
+# Calibrate the gyro to eliminate drift, and to initialize the current angle as 0
+tank.gyro.calibrate()
+
+kp = 10.0 #determines aggressiveness in corrections, can cause oscillations
+ki = 0.15  #attempts to eliminate drift over time
+kd = 3.0 #dampens the system response to oscillations
+RPS = 1
+
+def distance_to_time(distance_inches):
+    distance_mm = 25.4 * distance_inches #mm
+    circumference = 175.924 #mm / rotation
+    rotations = distance_mm / circumference 
+    time = 1000 * rotations
+    return time
+
+
+def straight(time):
+    tank.gyro.calibrate
+    tank.follow_gyro_angle(kp, ki, kd, speed=SpeedRPS(RPS), target_angle=0, follow_for=follow_for_ms, ms= time)
+
+def turn_left():
+    tank.turn_left(SpeedRPS(RPS), 90)
+def turn_right():
+    tank.turn_right(SpeedRPS(RPS), 90)
+    
+    
 def main():
-    conversion_factor = 25.4
-    kp = 10.0
-    ki = 0
-    kd = 3.0
-    basespeed = 50
-    target_angle = 0
-    mdiff = MoveDifferential(OUTPUT_B, OUTPUT_A, EV3EducationSetTire, 6.1*25.4)
-    mdiff.gyro = GyroSensor(INPUT_1)
-    distance = float(input("Enter the distance in inches >> "))
-    distance = distance * conversion_factor
-    for _ in distance:
-        mdiff.follow_gyro_angle(kp, ki, kd, basespeed, target_angle, sleep_time = 0.01 ) #sleep_time is in seconds
+    distance_inches = int(input("Enter the distance in inches >> "))
+    time = distance_to_time(distance_inches)
+    straight(time)
 main()
--- Code/Simulation.py
+++ Code/Simulation.py
@@ -1,6 +1,7 @@
 # Constants
 conversion_factor = 1
-box_x = 6 * conversion_factor
+box_x = 4 * conversion_factor
+box_x_gap = 2 * conversion_factor
 box_y = 6 * conversion_factor
 shelf_x = 36 * conversion_factor
 shelf_y = 12 * conversion_factor
@@ -28,72 +29,32 @@ def get_box_coordinates(shelf_label, box_number):
     
     # Calculate x_start, x_end based on box number
     if is_on_top(box_number):
-        x_start_box = x_start_shelf + ((box_number - 7) * box_x)
+        x_start_box = x_start_shelf + ((box_number - 7) * (box_x + box_x_gap)) + (1 * conversion_factor)
     else:
-        x_start_box = x_start_shelf + ((box_number - 1) * box_x)
+        x_start_box = x_start_shelf + ((box_number - 1) * (box_x + box_x_gap)) + (1 * conversion_factor)
     
     # The accessible edge y coordinate (bottom or top edge of the box)
     y_edge_box = y_start_shelf + shelf_y if is_on_top(box_number) else y_start_shelf
     print(f"{round(x_start_box, 2)}, {round(y_edge_box, 2)}")
     return x_start_box, y_edge_box
 
-def move_robot_to_box(target_x, target_y, box_number):
-    #assuming robot is a 6x6 cube starting at (0, 0)
-    robot_x = 3
-    robot_y = 3
-    current_orient = 0 #0 means north
-    
-    
-    error_rate_x = -0.04 * conversion_factor
-    error_rate_y = -0.48 * conversion_factor
-    turn_error_degrees = -1.15 #negative numbers are to the left in this program
-
-    def calculate_x_drift(distance):
-        return distance * error_rate_x
-    
-    def calculate_y_drift(distance):
-        return distance * error_rate_y
-    
-    def move_horizontal(target_x):
-        nonlocal robot_x
-        distance = target_x - robot_x
-        drift = calculate_x_drift(distance)
-        adj_target_x = target_x + drift
-        robot_x = adj_target_x
-        
-    def move_vertical(target_y):
-        nonlocal robot_y
-        distance = target_y - robot_y
-        drift = calculate_y_drift(distance)
-        adj_target_y = target_y + drift
-        robot_y = adj_target_y
-    
-    move_horizontal(target_x)
-    orientation_degrees = apply_turn_error(orientation_degrees)
-    move_vertical(target_y)
-    print(f"{robot_x}, {robot_y}")
-userinput = input("Enter a shelf and box number seperated by an underscore (I.E. A1_1) >> ")
-splituserinput = userinput.split("_")
-shelf_number = (splituserinput[0])
-box_number = int(splituserinput[1])
-x_start, x_end, y_edge = get_box_coordinates(shelf_number, box_number)
-target_x = x_start - 3
-target_y = y_edge - 3
-move_robot_to_box(target_x, target_y, box_number)
-
-    move_vertical(target_y)
-    turn(90)
-    move_horizontal(target_x)
-    turn(0)        
-    print(f"{round(robot_x, 2)}, {round(robot_y, 2)}")
 
 def main():
-    userinput = input("Enter a shelf and box number seperated by an underscore (I.E. A1_1) >> ")
-    splituserinput = userinput.split("_")
-    shelf_number = (splituserinput[0])
-    box_number = int(splituserinput[1])
+    rawinput = input("Enter shelf number, box number, barcode, and destination seperated by an underscore (I.E. A1_1, 1, C) >> ")
+    split_by_comma = rawinput.split(",")
+    split_input_str = [element.strip() for element in split_by_comma]
+    
+    shelf_and_box = split_by_comma[0]
+    barcode = split_by_comma[1]
+    destination = split_by_comma[2]
+    
+    shelf_and_box_lst = shelf_and_box.split("_")
+    
+    shelf_number = (shelf_and_box_lst[0])
+    box_number = int(shelf_and_box_lst[1])
+    
+    print(f"{shelf_number}, {box_number}, {barcode}, {destination}")
     x_box_start, y_edge = get_box_coordinates(shelf_number, box_number)
-    target_x = x_box_start
-    target_y = y_edge 
-    move_robot_to_box(target_x, target_y, box_number)
+    x_box_start = x_box_start - (6*conversion_factor)
+    y_edge = y_edge + (6*conversion_factor)
 main()
