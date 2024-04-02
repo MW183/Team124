4def2e5456f657e99f9060fffb6499fa58d7a386
ekpay10 <161762498+ekpay10@users.noreply.github.com>
2024-04-02 18:46:24 -0400

    Update testing_med_motor.py

--- Code/testing_med_motor.py
+++ Code/testing_med_motor.py
@@ -1,1 +1,7 @@
+from ev3dev2.motor import OUTPUT_D, MoveTank, SpeedRPS, MediumMotor, SpeedPercent
+from ev3dev2.sensor.lego import GyroSensor
+
+medMotor = MediumMotor(OUTPUT_D)
+
+medMotor.on_for_degrees(SpeedPercent(100), -45)
 
