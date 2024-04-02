b9390603e85c032126d6899e7e140d1abc274a28
ekpay10 <161762498+ekpay10@users.noreply.github.com>
2024-04-02 18:18:53 -0400

    not surreee

--- Code/Color Sensor.py
+++ Code/Color Sensor.py
@@ -3,26 +3,37 @@
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank
-from time import sleep
+import time 
 import math
+from ev3dev2.display import Display
 
+
+disp = Display()
 color_sensor = ColorSensor(INPUT_1)
 color_sensor.calibrate_white()
+'''will write each box as a certain code combo: ex. A1 = 1666'''
+#COLOR_BLACK = 1
+#COLOR_WHITE = 6
 
-
-def barcode_reading(barcode_type, near = false)
-    barcode_dict = (1:'1666', 2:'1616',3:'1166',4:'1661') 
+barcode_dict = {1:'1666', 2:'1616', 3:'1166', 4:'1661'}
+def barcode_reading(barcode_type, near = False):
     color = '0000'
-    if near == true:
-        for i in range(len(color)):
-            color[i] = color[i] + color_sensor
-            i = i + 1
-    if color != '0000' && color == barcode_dict[barcode_type]
+    if near == True:
+        for i in range(len(barcode_dict[barcode_type])):
+            color = color + str(color_sensor.COLOR)
+    if color == barcode_dict[barcode_type]:
         return color
-    
+    if near == True:
+        if barcode_type != color:
+            disp.text_pixels(input("This is not the box"),x=0,y=64)
+            disp.update()
+            time.sleep(10)
+        elif barcode_type == color:
+            disp.text_pixels("This is the box",x=0,y=64)
+            disp.update()
+            time.sleep(10)
 
-#loop??/bool
-boxtype1=0
+'''boxtype1=0
 boxtype2=0
 boxtype3=0
 boxtype4=0
@@ -41,19 +52,4 @@ if color_sensor.COLOR_BLACK:
                 boxtype4=1
     elif color_sensor.COLOR_BLACK:
         boxtype3=1
-else:
-    #move like normal?
-
-#i have it so it keeps pausing i think
-    
-#black = 1, white = 6
-def barcode_reading(barcode_type, near = false):
-    barcode_dict = {1 : "1666", 2:"1616", 3:"1166", 4:"1661"}
-    color = "0000"
-    if near == true:
-        for i in range(len(color):
-            color[i] += color_sensor
-            #move forward 0.5in
-            i++ 
-    if color != "0000" && color == barcode_dict[barcode_type]
-        return color
+else:'''
