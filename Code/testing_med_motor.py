eadabda88382d3518eb1cfdea08a227f805f4b22
ekpay10 <161762498+ekpay10@users.noreply.github.com>
2024-04-07 21:10:16 -0400

    update

--- Code/testing_med_motor.py
+++ Code/testing_med_motor.py
@@ -4,6 +4,8 @@ from ev3dev2.motor import OUTPUT_D, MoveTank, SpeedRPS, MediumMotor, SpeedPercen
 from ev3dev2.sensor.lego import GyroSensor
 from ev3dev2.sound import Sound
 import time
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedRPS, follow_for_ms
+
 
 sound = Sound()
 sound.beep()
@@ -11,10 +13,17 @@ sound.beep()
 medMotor = MediumMotor(OUTPUT_D)
 medMotor.reset()
 
-medMotor.on_to_position(SpeedPercent(100), 0)
-time.sleep(5)
+tank = MoveTank(OUTPUT_B, OUTPUT_A)  # left, right
+tank.gyro = GyroSensor()
+tank.gyro.calibrate()
+
+#medMotor.on_to_position(SpeedPercent(100), 0)
+#time.sleep(5)
+
+
+medMotor.on_to_position(SpeedPercent(100), -20)
+time.sleep(2)
+#tank.on_for_seconds(SpeedPercent(20), .5)
 medMotor.on_to_position(SpeedPercent(100), 180)
-time.sleep(5)
-medMotor.on_to_position(SpeedPercent(100), 0)
 
 exit()
