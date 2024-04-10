eea84e8258b22ea7c219b5341cf5a2046c04cffb
MW183 <mikeweinstein183@gmail.com>
2024-04-10 11:35:15 -0400

    Created code for first two subtasks

---
+++ Code/Final_Subtask1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedRPS, follow_for_ms
+from ev3dev2.sensor.lego import GyroSensor
+
+from time import sleep
+
+tank = MoveTank(OUTPUT_B, OUTPUT_A)  # left, right
+tank.gyro = GyroSensor()
+tank.gyro.calibrate()
+
+RPS = 1  # rotations per second
+
+conversion_factor = 25.4 #mm to in
+
+
+def straight(distance):
+    circumference = 6.92614 *conversion_factor  # Wheel circumference in mm
+    rotations = distance / circumference
+    time = 1000 * rotations
+    
+    # PID constants
+    kp = 10.0  # determines aggressiveness in corrections, can cause oscillations
+    ki = 0.15  # attempts to eliminate drift over time
+    kd = 3.0  # dampens the system response to oscillations
+
+    tank.gyro.reset()
+    tank.follow_gyro_angle(kp, ki, kd, SpeedRPS(RPS), target_angle=0, follow_for=follow_for_ms, ms=time)
+    
+def turn_left():
+    tank.turn_left(SpeedRPS(RPS), 90)
+    
+
+def turn_right():
+    tank.turn_right(SpeedRPS(RPS), 90)
+
+def turn_around():
+    tank.turn_right(SpeedRPS(RPS), 180)
+    
+def main():
+    box_number = int(input("Enter the box number for Shelf A1 >> "))
+    target_x = (16 + (6 * (box_number - 7))) * conversion_factor
+    target_y = 16 * conversion_factor
+    #x_offset = add measurement to midpoint for x
+    #y_offset = add measurement to midpoint for y 
+    
+    current_x = (6 * conversion_factor) #+ x_offset
+    current_y = (-6 * conversion_factor) #+ y_offset
+    
+    distance_x = target_x - current_x
+    distance_y = target_y - current_y
+    
+    straight(distance_x)
+    turn_right
+    straight(distance_y)
+    sleep(5)
+    
+    current_x = target_x
+    current_y = target_y
+    
+    target_x = 102 * conversion_factor
+    target_y = -6 * conversion_factor
+    
+    distance_x = target_x - current_x
+    distance_y = target_y - current_y
+    
+    straight(distance_x)
+    turn_right()
+    straight(distance_y)
+main()
+    
